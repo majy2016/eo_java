8d290515896a351a835f76a49469ca2258048217
majiayang <372840680@qq.com>
2019-12-02 14:00:30 +0800

    提交

---
+++ frontend_source_code/src/app/app.conf.js
@@ -0,0 +1,8 @@
+(function () { 
+ return angular.module("eolinker")
+.constant("serverUrl", "/eolinker_os/")
+.constant("isDebug", true)
+.constant("assetUrl", "app/")
+.constant("COOKIE_CONFIG", {"path":"/","domain":"localhost"});
+
+})();
